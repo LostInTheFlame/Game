a8c87284533e6171d1b0eb3f937327be8525dca4
Maksim <xSeptentrioNx@gmail.com>
2020-05-26 20:39:03 +0300

    Version 1.0

---
+++ Game/Program.cs
@@ -0,0 +1,150 @@
+﻿using System;
+
+namespace Game
+{
+    class Program
+    {
+        static bool endFlag = false;
+        static int x = 1;
+        static int y = 1;
+        static char sprite = 'P';
+        static int life = 10;
+
+        static void Main()
+        {
+            Console.CursorVisible = false;
+            Field();
+            Random rand = new Random();
+            int[] trapCoordinates = new int[20];
+            for (int i = 0; i < 20; i++)
+            {
+                trapCoordinates[i] = rand.Next(1, 10);
+            }
+
+            while (true)
+            {
+                PlayerDraw();
+                switch (Console.ReadKey(true).Key)
+                {
+                    case ConsoleKey.LeftArrow:
+                        if (x <= 1)
+                        {
+                            continue;
+                        }
+                        PlayerRemove();
+                        x--;
+                        break;
+                    case ConsoleKey.RightArrow:
+                        if (x >= 10)
+                        {
+                            continue;
+                        }
+                        PlayerRemove();
+                        x++;
+                        break;
+                    case ConsoleKey.UpArrow:
+                        if (y <= 1)
+                        {
+                            continue;
+                        }
+                        PlayerRemove();
+                        y--;
+                        break;
+                    case ConsoleKey.DownArrow:
+                        if (y >= 10)
+                        {
+                            continue;
+                        }
+                        PlayerRemove();
+                        y++;
+                        break;
+                }
+
+                for (int i = 0, j = 1; i < 20; i += 2, j += 2)
+                {
+                    if (x == trapCoordinates[i] && y == trapCoordinates[j])
+                    {
+                        life -= rand.Next(1, 10);
+                        Console.SetCursorPosition(0, 14);
+                        Console.Write($"You're trapped. Remaimning life: {life}");
+                    }
+
+                    if (life < 1)
+                    {
+                        End();
+                    }
+                }
+
+                if (x == 10 && y == 10)
+                {
+                    endFlag = true;
+                    End();
+                }
+            }
+
+            static void Field()
+            {
+                char ch = '*';
+                for (int i = 0, j = 0; i < 12; i++, j++)
+                {
+                    Console.SetCursorPosition(i, 0);
+                    Console.Write(ch);
+                    Console.SetCursorPosition(i, 11);
+                    Console.Write(ch);
+                    Console.SetCursorPosition(0, j);
+                    Console.Write(ch);
+                    Console.SetCursorPosition(11, j);
+                    Console.Write(ch);
+                }
+                Console.SetCursorPosition(10, 10);
+                Console.WriteLine("?");
+            }
+
+            static void PlayerDraw()
+            {
+                Console.SetCursorPosition(x, y);
+                Console.Write(sprite);
+            }
+
+            static void PlayerRemove()
+            {
+                Console.SetCursorPosition(x, y);
+                Console.Write(" ");
+            }
+
+            static void End()
+            {
+                Console.Clear();
+                if (endFlag == true)
+                {
+                    Console.WriteLine("Congratulations, you won!\nWant to play again? (y/n)");
+                }
+                else
+                {
+                    Console.WriteLine("Sorry, you lost :(\nWant to try again? (y/n)");
+                }
+
+                string tryAgain = Console.ReadLine();
+                switch (tryAgain)
+                {
+                    case "y":
+                        Console.Clear();
+                        x = 1;
+                        y = 1;
+                        life = 10;
+                        endFlag = false;
+                        Main();
+                        break;
+
+                    case "n":
+                        Environment.Exit(0);
+                        break;
+
+                    default:
+                        End();
+                        break;
+                }
+            }
+        }
+    }
+}
